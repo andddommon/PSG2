7adcdd7d6e891b45d0b3aa4824cf9688135e23e0
David Romero <drorganvidez@outlook.com>
2019-03-05 16:29:25 +0100

    Añadida funcionalidad de eliminar una mastoca Closes #1
    Pendiente de probar

--- src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
@@ -39,6 +39,8 @@ public interface ClinicService {
     Pet findPetById(int id) throws DataAccessException;
 
     void savePet(Pet pet) throws DataAccessException;
+    
+    void removePet(Pet pet) throws DataAccessException;
 
     void saveVisit(Visit visit) throws DataAccessException;
 
